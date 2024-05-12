d4ce369ce383f1f6ac69f9e6c7ef6bd20a8551b3
GreatGrace24 <157252634+GreatGrace24@users.noreply.github.com>
2024-05-12 21:56:59 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#E94339",
+        secondary: ' #FAFAFA',
+        primaryText: "#191720"
+      }
+    },
+  },
+  plugins: [],
+};
